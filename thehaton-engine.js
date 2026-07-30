dfd01a3329e583b0416f7aa37425bb73b79de9e3
davidariel631-art <davidariel631@gmail.com>
2026-07-30 05:39:52 -0300

    Add files via upload

--- thehaton-engine.js
+++ thehaton-engine.js
@@ -26,8 +26,8 @@ const TF_MAP = {
 
 const CORS_PROXIES = [
   url => `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`,
-  url => `https://corsproxy.io/?url=${encodeURIComponent(url)}`,
-  url => `https://thingproxy.freeboard.io/fetch/${url}`,
+  url => `https://api.codetabs.com/v1/proxy/?quest=${encodeURIComponent(url)}`,
+  url => `https://corsfix.com/${url}`,
 ];
 
 async function fetchJSON(url){
